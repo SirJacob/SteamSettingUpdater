426270435b427d57d996987e30f2384ef4dfeff9
SirJacob <SirJacob@users.noreply.github.com>
2019-10-27 20:04:27 -0400

    Now automatically finds other Steam libraries when Steam is installed in it's default location, otherwise user is prompted for installation path.

--- SteamAutoUpdateChanger.py
+++ SteamAutoUpdateChanger.py
@@ -1,24 +1,50 @@
-import glob
-import os
-
-all_steamapps_directories = [
-    "C:\\Program Files (x86)\\Steam\\steamapps"
-]
-
 """
+TODO: Implement checking to see if Steam is open.
+TODO: If Steam is open, prompt to close automatically.
+TODO: Menu system
+
 AutoUpdateBehavior Settings:
 0 = Always keep this game up to date
 1 = Only update this game when I launch it
 2 = High Priority - Always auto-update this game before others
 """
+import glob
+import os
+import GeneralTools as Tools
+import VDFReader
 
+all_steam_directories = [
+    "C:\\Program Files (x86)\\Steam"  # The default Steam installation directory
+]
+
+# Let's find out if this system has a custom Steam installation directory
+# TODO: Find an alternative Steam installation directory when not default w/o user input
+while not Tools.folder_exists(all_steam_directories[0]):
+    all_steam_directories[0] = input("Please provide the path to your Steam installation: ")
+# Let's find out if this system has more than one Steam library setup
+# Load library folder VDF into memory
+library_folders_vdf = VDFReader.VDFReader(f"{all_steam_directories[0]}\\steamapps\\libraryfolders.vdf")
+
+# Add all Steam libraries we found in libraryfolders.vdf
+index = 1
+while index >= 1:
+    alt_steam_library = library_folders_vdf.get(index)
+    if alt_steam_library is None:
+        index = -1
+    else:
+        all_steam_directories.append(alt_steam_library)
+        index += 1
+
+# Variables for keeping track of how many acf files were processed
 num_updated_acf_files = 0
 num_total_acf_files = 0
 
-for steamapp_directory in all_steamapps_directories:
-    os.chdir(steamapp_directory)
+# TODO: Cleanup
+for steamapp_directory in all_steam_directories:
+    os.chdir(steamapp_directory + "\\steamapps")
     for appmanifest_path in glob.glob("appmanifest_*.acf"):
         num_total_acf_files += 1
+
         appmanifest = open(appmanifest_path, "r+")
         appmanifest_contents = appmanifest.read()
         if appmanifest_contents.__contains__('"AutoUpdateBehavior"		"0"'):
@@ -33,4 +59,4 @@ for steamapp_directory in all_steamapps_directories:
 print("AutoUpdateBehavior from 0 to 1")
 print("------------------------------")
 print(f"{num_updated_acf_files}/{num_total_acf_files} ACF files updated"
-      f" in {len(all_steamapps_directories)} directories.")
+      f" in {len(all_steam_directories)} directories.")
